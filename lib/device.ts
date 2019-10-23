a19a3a935a44c4ad63fc7d7dc8c595f6818dfb45
梁柱 <ymblender@gmail.com>
2019-10-23 18:51:15 +0800

    1

--- lib/device.ts
+++ lib/device.ts
@@ -15,3 +15,6 @@ export const isSmall = dp > 1 ? iw <= 320 : iw <= 640;
 export const isMiddle = dp > 1 ? iw <= 375 : iw <= 750;
 export const isLarge = dp > 1 ? iw <= 512 : iw <= 1024;
 export const isExtraLarge = dp > 1 ? iw <= 640 : iw <= 1280;
+export const isNativeIOS = window.location.href.indexOf('_os_ios_') >= 0;
+export const isNativeAndroid = window.location.href.indexOf('_os_android_') >= 0;
+export const isNative = () => isNativeIOS || isNativeAndroid || (window as any).cordova || false;
